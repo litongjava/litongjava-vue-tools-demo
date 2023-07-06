508d6c545a35aba1c71190310cb1b8552097f339
litongjava <litongjava@qq.com>
2023-07-06 19:42:41 +0800

    封装完成

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LitongjavaVueTools from 'litongjava-vue-tools';
+Vue.use(LitongjavaVueTools);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
+  render: function (h) {
+    return h(App)
+  },
+}).$mount('#app');
